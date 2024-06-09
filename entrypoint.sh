bf64480c062a2b2f144dc5c1533edd33470f52e4
Jordan J <jordan@seddona.com>
2024-06-09 00:45:48 -0700

    try timeout import

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,8 @@ echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
 
-godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 -v --quit
+timeout 10s godot --headless --import $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
+godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
 echo "Build Done"
 
 echo ::set-output name=build::build/${SubDirectoryLocation:-""}
