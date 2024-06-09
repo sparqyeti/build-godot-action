b8148b13cde7153e74141d3627f2a3563c50c758
Jordan J <jordan@seddona.com>
2024-06-09 00:23:30 -0700

    no import

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,8 +23,6 @@ echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
 
-timeout 10s godot --headless --import $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
-
 godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 -v --quit
 echo "Build Done"
 
